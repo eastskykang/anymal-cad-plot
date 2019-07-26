66c338403a08ada1ae7686728e59d7f6ea21973e
donghokang <east0822@gmail.com>
2019-07-26 17:36:16 +0200

    tcn1 added

--- success_plot.m
+++ success_plot.m
@@ -8,17 +8,20 @@
 
 cnn_file = 'cnn.csv';
 lstm_file = 'lstm.csv';
-tcn_file = 'tcn.csv';
+tcn_file1 = 'tcn1.csv';
+tcn_file2 = 'tcn2.csv';
 
 cnn_t = readtable(cnn_file);
 lstm_t = readtable(lstm_file);
-tcn_t = readtable(tcn_file);
+tcn_t1 = readtable(tcn_file1);
+tcn_t2 = readtable(tcn_file2);
 
 figure('Position',[0 0 800 600])
 plot(cnn_t.stage, cnn_t.successRate, 'r', 'Linewidth', 2)
 hold on
 plot(lstm_t.stage, lstm_t.successRate, 'g', 'Linewidth', 2)
-plot(tcn_t.stage, tcn_t.successRate, 'b', 'Linewidth', 2)
+plot(tcn_t1.stage, tcn_t1.successRate, 'c', 'Linewidth', 2)
+plot(tcn_t2.stage, tcn_t2.successRate, 'b', 'Linewidth', 2)
 hold off
 ax1 = gca;  % current axes
 ax1.FontSize = 20;
@@ -33,40 +36,43 @@ legend('CNN policy', 'LSTM policy', 'TCN policy (2 TCN layer)', 'location', 'sou
 
 cnn_file = 'aecnn-5hz.csv';
 lstm_file = 'aelstm-5hz.csv';
-tcn_file = 'aetcn-2layer-5hz.csv';
+tcn_file1 = 'aetcn-1layer-5hz.csv';
+tcn_file2 = 'aetcn-2layer-5hz.csv';
 
 cnn_t = readtable(cnn_file);
 lstm_t = readtable(lstm_file);
-tcn_t = readtable(tcn_file);
-
-cnn_t = readtable(cnn_file);
-lstm_t = readtable(lstm_file);
-tcn_t = readtable(tcn_file);
+tcn_t1 = readtable(tcn_file1);
+tcn_t2 = readtable(tcn_file2);
 
 max_timestep = 2190;
 
 cnn_eprew = cnn_t.eprew(1:max_timestep);
 lstm_eprew = lstm_t.eprew(1:max_timestep);
-tcn_eprew = tcn_t.eprew(1:max_timestep);
+tcn_eprew1 = tcn_t1.eprew(1:max_timestep);
+tcn_eprew2 = tcn_t2.eprew(1:max_timestep);
 
 step = 1:max_timestep;
 [cnn_eprew, cnn_mask] = rmoutliers(cnn_eprew);
 [lstm_eprew, lstm_mask] = rmoutliers(lstm_eprew);
-[tcn_eprew, tcn_mask] = rmoutliers(tcn_eprew);
+[tcn_eprew1, tcn_mask1] = rmoutliers(tcn_eprew1);
+[tcn_eprew2, tcn_mask2] = rmoutliers(tcn_eprew2);
 
 cnn_step = step(~cnn_mask);
 lstm_step = step(~lstm_mask);
-tcn_step = step(~tcn_mask);
+tcn_step1 = step(~tcn_mask1);
+tcn_step2 = step(~tcn_mask2);
 
 cnn_eprew = tsmovavg(cnn_eprew, 's', 3, 1);
 lstm_eprew = tsmovavg(lstm_eprew, 's', 3, 1);
-tcn_eprew = tsmovavg(tcn_eprew, 's', 3, 1);
+tcn_eprew1 = tsmovavg(tcn_eprew1, 's', 3, 1);
+tcn_eprew2 = tsmovavg(tcn_eprew2, 's', 3, 1);
 
 figure('Position',[0 0 800 600])
 plot(cnn_step, cnn_eprew, 'r', 'Linewidth', 2)
 hold on
 plot(lstm_step, lstm_eprew, 'g', 'Linewidth', 2)
-plot(tcn_step, tcn_eprew, 'b', 'Linewidth', 2)
+plot(tcn_step1, tcn_eprew1, 'b', 'Linewidth', 2)
+plot(tcn_step2, tcn_eprew2, 'b', 'Linewidth', 2)
 hold off
 ax1 = gca;  % current axes
 ax1.FontSize = 20;
